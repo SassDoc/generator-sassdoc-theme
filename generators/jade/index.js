d9bda7b2a11fc3b6eccb9c4064f3676470a4e1db
Valérian Galliat <val@codejam.info>
2015-01-30 18:48:40 +0100

    Update the generators and test script to use `sassdoc-extras`

--- generators/jade/index.js
+++ generators/jade/index.js
@@ -9,8 +9,7 @@ var isset = require('../../utils').isset;
 var Generator = module.exports = function Generator() {
   yeoman.generators.Base.apply(this, arguments);
 
-  this.useFilter = isset(this.options.useFilter) ? this.options.useFilter : true;
-  this.useIndexer = isset(this.options.useIndexer) ? this.options.useFilter : true;
+  this.useExtras = isset(this.options.useExtras) ? this.options.useExtras : true;
 };
 
 util.inherits(Generator, yeoman.generators.Base);
@@ -19,14 +18,9 @@ Generator.prototype.buildViews = function createViewFiles() {
   this.sourceRoot(path.join(__dirname, '../../templates/jade'));
 
   var index = 'index.jade';
-  if (!this.useFilter && !this.useIndexer) {
+
+  if (!this.useExtras) {
     index = 'index_bare.jade';
-  }
-  if (this.useFilter && !this.useIndexer) {
-    index = 'index_filter.jade';
-  }
-  if (!this.useFilter && this.useIndexer) {
-    index = 'index_indexer.jade';
   }
 
   this.mkdir('views');
--- generators/nunjucks/index.js
+++ generators/nunjucks/index.js
@@ -9,8 +9,7 @@ var isset = require('../../utils').isset;
 var Generator = module.exports = function Generator() {
   yeoman.generators.Base.apply(this, arguments);
 
-  this.useFilter = isset(this.options.useFilter) ? this.options.useFilter : true;
-  this.useIndexer = isset(this.options.useIndexer) ? this.options.useFilter : true;
+  this.useExtras = isset(this.options.useExtras) ? this.options.useExtras : true;
 };
 
 util.inherits(Generator, yeoman.generators.Base);
@@ -19,14 +18,9 @@ Generator.prototype.buildViews = function createViewFiles() {
   this.sourceRoot(path.join(__dirname, '../../templates/nunjucks'));
 
   var index = 'index.nunjucks';
-  if (!this.useFilter && !this.useIndexer) {
+
+  if (!this.useExtras) {
     index = 'index_bare.nunjucks';
-  }
-  if (this.useFilter && !this.useIndexer) {
-    index = 'index_filter.nunjucks';
-  }
-  if (!this.useFilter && this.useIndexer) {
-    index = 'index_indexer.nunjucks';
   }
 
   this.mkdir('views');
